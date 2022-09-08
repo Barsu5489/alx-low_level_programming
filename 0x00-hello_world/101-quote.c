80ef92e0bd02a2969d5ab63289b349e654e78ee9
Barsu5489 <ebarsulai@gmail.com>
2022-09-08 14:00:47 -0700

    simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,38 +1,23 @@
-#include <stdio.h>                                                                                                                      
-
-                       
-
-
-
-                                                                                                                   
-
-                                                                       
-                                                                                                      
-                                                                                                 
-                                                                                                                               
-
-		
+nclude <stdio.h>
 
 /**
-*
 * main - prints to string
-*
 * Description: Prints "and that piece of art is useful.." without puts
-*
 * Return: 1
-*/		
+*/
 int main(void)
 {
-char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";		
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 long l = 59;
 long fd = 1;
-long syscall = 1;		
-long ret = 0;		
-__asm__ ("syscall"		
+long syscall = 1;
+long ret = 0;
+__asm__ ("syscall"
 : "=a" (ret)
 : "a" (syscall),
 "D" (fd),
-"S" (s),				
+"S" (s),
 "d" (l));
-return (1);                                                                           
-}                                                                                                 
+return (1);
+
+} 
