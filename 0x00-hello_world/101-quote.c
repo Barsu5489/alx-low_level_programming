39e5ef09600a90468528216b5ce7dc005404b366
Barsu5489 <ebarsulai@gmail.com>
2022-09-08 13:23:45 -0700

    simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>                                                                                                                      
+
+                                                                                                                                        
+
+/**                                                                                                                                     
+ *
+ *main - Entry block                                                                                                                    
+ *
+ *Description:'the program's description'                                                                          
+ * Return: Always 0 (Success)*                                                                                                         
+    */                                                                                                    
+int main(void){                                                                                                                                       
+puts("\"Programming is like building a multilingual puzzle");                                                                           
+return (0);                                                                                                              
+}
