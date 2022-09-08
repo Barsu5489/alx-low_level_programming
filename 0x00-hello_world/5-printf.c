8d003f2160855efb8122235090d0349902bd82a1
Barsu5489 <ebarsulai@gmail.com>
2022-09-08 12:27:07 -0700

    proper grammer

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
 * main - Print a string
 * Description: use printf
-* Return: 0
+* Return:always 0 (success)
 */
 
 int main(void)
 
 {
-	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");		return (0);
+printf("\"with proper grammar, but the outcome is a piece of art,\n");		return (0);
 }
