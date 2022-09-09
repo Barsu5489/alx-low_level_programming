1ecf2aa0211fc19a0a400cf10725258f3b98c396
Barsu5489 <ebarsulai@gmail.com>
2022-09-09 12:22:16 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char a-z;
+	for (a-z = 122; a-z >= 97; a-z--)
+{
+putchar(a-z);
+}
+putchar('\n');
+return (0);
+}
+	
