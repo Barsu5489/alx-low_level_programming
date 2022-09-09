62bd262229d3cacd08753bb6e521175307acad3e
Barsu5489 <ebarsulai@gmail.com>
2022-09-09 08:48:03 -0700

    skip q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char letters;
+for (letters = 'a'; letters <= 'z'; letters++)
+{
+if (letters == q && letters == e)
+{
+continue;
+}
+else
+{
+putchar(letters);
+}
+putchar("\n");
+return (0);
+}
+
