e4192d24dee47a767fe95089fdf2ec7280a4191f
Barsu5489 <ebarsulai@gmail.com>
2022-09-13 05:10:34 -0700

    check for lowercase characters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *int _islower(int c) - _islower
+ *
+ *Return: 0
+ */
+int _islower(int c)
+{	
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+	_putchar('\n');
+		return (0);
+}
