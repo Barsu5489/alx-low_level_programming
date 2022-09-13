6d58c9847f624fc5cdfb5e6d7f9971cfd2a31921
Barsu5489 <ebarsulai@gmail.com>
2022-09-13 02:52:34 -0700

    insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabate - func
+ *
+ */
+
+void print_alphabet(void)
+{
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+_putchar(alphabet);
+alphabet++;
+}
+_putchar('\n')
+}
