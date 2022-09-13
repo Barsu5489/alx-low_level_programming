23d02f072d5fcec341bfbec0c379fbeb981fff37
Barsu5489 <ebarsulai@gmail.com>
2022-09-13 07:39:57 -0700

    upper and lower print

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,6 +2,7 @@
 
 /**
  *_isalpha - contains parameter of int c
+ * @c: print upper and lower
  *Description: Returns 1 if c is lowercase
  *Return: 0
  */
@@ -9,7 +10,7 @@
 int _isalpha(int c)
 {
 
-	if (c >= 'a' ||	c <= 'z' || c >= 'A' || c <= 'Z')
+	if (c >= 'a' &&	c <= 'z' || c >= 'A' && c <= 'Z')
 
 	{
 
