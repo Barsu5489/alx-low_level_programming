cb3cf5d94c0e246620c4853f732ac0f3a1294d79
Barsu5489 <ebarsulai@gmail.com>
2022-09-13 01:52:02 -0700

    print character

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "_putchar"
+/**
+ * main - main
+ * Description: A program that prints putchar
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	char c[8]  = "_putchar";
+	for( i = 0; i <= 7; i++)
+	{
+	putchar(c[i]);
+	putchar('\n');
+	}
+	return 0;
+}
