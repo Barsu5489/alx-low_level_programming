b8c44d1e1e8ef22339e5a2b3d28c9d90aa7e7df2
Barsu5489 <ebarsulai@gmail.com>
2022-09-13 02:06:51 -0700

    fix update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "_putchar"
 /**
  * main - main
  * Description: A program that prints putchar
@@ -10,8 +10,8 @@ int main(void)
 	char c[8]  = "_putchar";
 	for( i = 0; i <= 7; i++)
 	{
-	putchar(c[i]);
-	putchar('\n');
+	_putchar(c[i]);
+	_putchar('\n');
 	}
 	return 0;
 }
