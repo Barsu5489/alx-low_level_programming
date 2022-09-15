7286d69fdad1d32a4bd09a5ced699ad82d290cbd
Barsu5489 <ebarsulai@gmail.com>
2022-09-15 07:39:44 -0700

    square

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,6 +11,7 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+void print_square(int size);
 int largest_number(int, int, int);
 
 void print_remaining_days(int, int, int);
