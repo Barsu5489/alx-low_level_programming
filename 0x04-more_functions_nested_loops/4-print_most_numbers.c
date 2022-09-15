36196f3dd4335a9fddf91cda10db132c7da79ab6
Barsu5489 <ebarsulai@gmail.com>
2022-09-15 05:36:22 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,13 +6,12 @@
 
 void print_most_numbers(void)
 {
-int a = 0;
-while (a <= 9)
+	int a;
+for (a = 0; a <= 9; a++)
 {
 if (a == 2 || a == 4)
 	continue;
 	_putchar(a);
-	a++;
 }
 _putchar('\n');
 }
