e446d3dcf51c5e693c2ac0bf2632280b7b0fdd47
Barsu5489 <ebarsulai@gmail.com>
2022-09-15 04:29:45 -0700

    quick fix

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,1 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _isupper(int c);
+void positive_or_negative(int i);
+
+int largest_number(int, int, int);
+
+void print_remaining_days(int, int, int);
+
+int convert_day(int, int);
+
+
+
+#endif /* MAIN_H */ 
