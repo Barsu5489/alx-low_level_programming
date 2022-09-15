e37b7b9303516e75fd86f16b6f11c576d7d15d40
Barsu5489 <ebarsulai@gmail.com>
2022-09-15 05:47:46 -0700

    print most quick fix

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void positive_or_negative(int i);
-int _putchar(char c);
 void print_most_numbers(void);
 int largest_number(int, int, int);
 
