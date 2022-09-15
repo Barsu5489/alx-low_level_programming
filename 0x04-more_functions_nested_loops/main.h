26c7826b80b65a6f91081f5d1d713e50c2d06f77
Barsu5489 <ebarsulai@gmail.com>
2022-09-15 12:53:34 -0700

    symbols

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -15,7 +15,7 @@ void print_square(int size);
 int largest_number(int, int, int);
 void print_triangle(int size);
 void print_remaining_days(int, int, int);
-
+void print_number(int n);
 int convert_day(int, int);
 
 
