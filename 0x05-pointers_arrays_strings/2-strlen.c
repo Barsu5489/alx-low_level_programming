0d2a2b0f3b625167d6572880ea5e09c9c759e437
Barsu5489 <ebarsulai@gmail.com>
2022-09-19 09:01:21 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - return length of s string
+ * @s: paremeter
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	for (l = 0; l <= *(s + l); l++)
+	return (l);
+
+}
