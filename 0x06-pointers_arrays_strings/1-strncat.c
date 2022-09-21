61064aef4650da51ecd62abc2e927eac72caab7d
Barsu5489 <ebarsulai@gmail.com>
2022-09-21 15:29:41 -0700

    append n characters

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - function to append specific number of bytes
+ * @dest: destination
+ * @src: source
+ * @n: size of bytes
+ *
+ * Return: 0
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] += src[j];
+		i++;
+		j++;
+	}
+	dest += '\0';
+	return (dest);
+
+}
+}
