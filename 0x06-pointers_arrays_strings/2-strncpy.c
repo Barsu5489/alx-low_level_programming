c1fc98dddd81e0fdc63fa1eb379f877b6a55cce2
Barsu5489 <ebarsulai@gmail.com>
2022-09-22 11:02:36 -0700

    complete pointers project

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,22 @@
 #include "main.h"
 
 /**
- * _strncpy - function that copies a string
- *
+ * _strncpy - function that copies a string of size n
+ * @dest: destination param
+ * @src: source param
+ * @n: size of string to be copied
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+	dest[i] = src[i];
+	}
+	for (i < n; i++)
+	dest[i] = '\0';
+
+	return (dest);
+}
