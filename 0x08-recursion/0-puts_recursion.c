6bdafa60ff00a7de8cd7fadfc943697405a67879
Barsu5489 <ebarsulai@gmail.com>
2022-09-28 09:13:53 -0700

    recersion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints string followed by new line.
+ *
+ * @s: pointer to string
+ *
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	_putchar('\n');
+	return;
+	}
+	else
+	{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	}
+}
