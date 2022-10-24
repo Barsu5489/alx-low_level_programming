49577ac5f9f499b8364538f2b068599a3ff74ee0
Barsu5489 <ebarsulai@gmail.com>
2022-10-24 08:23:37 -0700

    simple assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,16 @@
-section     .text
-    global      main
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+SECTION .text
+extern printf
+global main
 
 main:
+mov esi, msg
+mov edi, fmt
+mov eax, 0
+call printf
+mov eax, 0
 
-    mov     edx,len
-    mov     ecx,msg
-    mov     ebx,1
-    mov     eax,4
-    int     0x80
-
-section     .data
-msg     db  'Hello, Holberton',0xa
-len     equ $ - msg
+ret
