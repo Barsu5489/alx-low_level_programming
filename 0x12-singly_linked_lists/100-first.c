f887746633c89f18b552480a1cb5d5ebf744749a
Barsu5489 <ebarsulai@gmail.com>
2022-10-24 08:09:08 -0700

    sungura fala

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,12 +3,10 @@
 /**
  * premain - run before main
  */
-
 void __attribute__ ((constructor)) premain()
-
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf(
+			"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+	      );
 }
-
 
