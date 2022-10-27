cfd5d00b838869841bb9a91214f440a8737ceb64
Barsu5489 <ebarsulai@gmail.com>
2022-10-27 12:46:17 -0700

    adds int the beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,14 +7,23 @@
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
+
 {
-	listint_len *new_node = malloc(sizeof(listint_t));
 
-	if (!new_node)
-		return (Null);
-	new_node->n = n;
-	new_node->next = (*head);
-	(*head) = newnode;
+	listint_t *new_node;
 
-	return  (*head);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node != NULL)
+	{
+		new_node->n = n;
+		new_node->next = *head;
+	}
+	else
+
+		return (NULL);
+	if (*head != NULL)
+		new_node->next = *head;
+		*head = new_node;
+	return (new_node);
+
 }
