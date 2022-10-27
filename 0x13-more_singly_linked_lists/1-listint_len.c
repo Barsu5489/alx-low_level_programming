f472c38735af946a0f72f45f42431c2b198cafbd
Barsu5489 <ebarsulai@gmail.com>
2022-10-27 11:57:44 -0700

    count number of "nudes"

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,15 +4,15 @@
  * @h: pointer to header
  * Return:count
  */
- 
+
 size_t listint_len(const listint_t *h)
 {
 	const listint_t *n = h;
 	size_t count = 0;
 
-	while (h !=NULL)
+	while (h != NULL)
 	{
-		count ++;
+		count++;
 		n = h->next;
 	}
 	return (count);
