fe10e8a6554f6595ed0fd7b890576b54f8298b7e
Barsu5489 <ebarsulai@gmail.com>
2022-10-27 13:37:18 -0700

    add start

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,6 +4,8 @@
  * *add_nodeint - Adds new node at beginning of list
  * @head: pointer to pointer of head of first
  * @n : integer
+ *
+ * Return: new node
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
